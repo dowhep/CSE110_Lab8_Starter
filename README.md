a27a69e7800e6d2aac141def0b29cb73cf2e8543
Marcelo Shen <caogangshen@gmail.com>
2023-05-29 00:59:24 -0700

    Explore

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Lab 8 - Starter
 
+Written by: Me and myself.
+
 Check my understanding:
 
 1. I would fit my automated tests of my Recipe project development pipline by manually running them locally before pushing code because this is a relatively small project without the need of branch splitting, and the only way to ensure the main branch is correct is by verifying code before they are pushed.
 2. No.
-3. 
+3. No, because "message" feature requires multiple different moving parts and unit test is only designed to test individual parts.
+4. Yes, because "max message length" feature is an individual part, which unit test is designed for.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,83 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('test isPhoneNumber true 1', () => {
+  expect(functions.isPhoneNumber("(123)456-7890")).toBe(true);
+});
+
+test('test isPhoneNumber true 2', () => {
+  expect(functions.isPhoneNumber("123-456-7890")).toBe(true);
+});
+
+test('test isPhoneNumber false 1', () => {
+  expect(functions.isPhoneNumber("1,234,567,890")).toBe(false);
+});
+
+test('test isPhoneNumber false 2', () => {
+  expect(functions.isPhoneNumber("123456789.0")).toBe(false);
+});
+
+test('test isEmail true 1', () => {
+  expect(functions.isEmail("never@gonna.com")).toBe(true);
+});
+
+test('test isEmail true 2', () => {
+  expect(functions.isEmail("give@you.up")).toBe(true);
+});
+
+test('test isEmail false 1', () => {
+  expect(functions.isEmail("nevergonna")).toBe(false);
+});
+
+test('test isEmail false 2', () => {
+  expect(functions.isEmail("let@you.down")).toBe(false);
+});
+
+test('test isStrongPassword true 1', () => {
+  expect(functions.isStrongPassword("rickroll")).toBe(true);
+});
+
+test('test isStrongPassword true 2', () => {
+  expect(functions.isStrongPassword("inevitable")).toBe(true);
+});
+
+test('test isStrongPassword false 1', () => {
+  expect(functions.isStrongPassword("1ncredible")).toBe(false);
+});
+
+test('test isStrongPassword false 2', () => {
+  expect(functions.isStrongPassword("i")).toBe(false);
+});
+
+test('test isDate true 1', () => {
+  expect(functions.isDate("00/00/0000")).toBe(true);
+});
+
+test('test isDate true 2', () => {
+  expect(functions.isDate("0/0/0000")).toBe(true);
+});
+
+test('test isDate false 1', () => {
+  expect(functions.isDate("00000000")).toBe(false);
+});
+
+test('test isDate false 2', () => {
+  expect(functions.isDate("$$/##/%%%%")).toBe(false);
+});
+
+test('test isHexColor true 1', () => {
+  expect(functions.isHexColor("ABABFF")).toBe(true);
+});
+
+test('test isHexColor true 2', () => {
+  expect(functions.isHexColor("#BADAB0")).toBe(true);
+});
+
+test('test isHexColor false 1', () => {
+  expect(functions.isHexColor("short")).toBe(false);
+});
+
+test('test isHexColor false 2', () => {
+  expect(functions.isHexColor("#loooong")).toBe(false);
+});
